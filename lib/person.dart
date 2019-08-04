42e24adc76813073378b733d310759c8d88a6db0
akdombrowski <akdflyinvee73@gmail.com>
2019-08-04 11:49:28 -0500

    render person component

--- lib/person.dart
+++ lib/person.dart
@@ -4,12 +4,17 @@ import 'package:flutter_game/MyGame.dart';
 class Person {
   final MyGame game;
   Rect personRect;
+  Paint personPaint;
 
-Person(this.game, double x, double y) {
-  personRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
-}
+  Person(this.game, double x, double y) {
+    personRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
+    personPaint = Paint();
+    personPaint.color = Color(0xff6ab04c);
+  }
 
-  void render(Canvas c) {}
+  void render(Canvas c) {
+    c.drawRect(personRect, personPaint);
+  }
 
   void update(double t) {}
 }
