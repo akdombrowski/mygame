0f6273a2cb0d58a43d26c12f23d7683cfc107814
akdombrowski <akdflyinvee73@gmail.com>
2019-08-04 11:52:46 -0500

    create initialize method in main game loop

--- lib/MyGame.dart
+++ lib/MyGame.dart
@@ -1,10 +1,18 @@
 import 'dart:ui';
 import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
 
 class MyGame extends Game {
   Size screenSize;
   double tileSize;
 
+  MyGame() {
+    initialize();
+  }
+
+  void initialize() async {
+    resize(await Flame.util.initialDimensions());
+  }
 
   void render(Canvas canvas) {
     Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
@@ -19,6 +27,5 @@ class MyGame extends Game {
     screenSize = size;
     // for screens 16:9 or 16:x
     tileSize = screenSize.width / 16;
-
   }
 }
